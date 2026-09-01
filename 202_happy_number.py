6327881f5d99689a67261108c934f7d5c9d6f1a4
Vyshnavi <vyshnavireddy639@gmail.com>
2026-09-01 23:02:06 +0530

    add happy number solution

---
+++ 202_happy_number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isHappy(self, n):
+      
+        seen = set()
+    
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit) ** 2 for digit in str(n))
+    
+        return n == 1
+
+
+
+        
