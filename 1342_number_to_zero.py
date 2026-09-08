1936b27405cd51085d9354c68c423bc7cde364ef
Vyshnavi <vyshnavireddy639@gmail.com>
2026-09-09 00:28:30 +0530

    add steps to reduce a number to zero solution

---
+++ 1342_number_to_zero.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        steps = 0
+
+        while num:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num -= 1
+
+            steps += 1
+
+        return steps
